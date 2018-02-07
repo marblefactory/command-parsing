639f01ffd1136f5288953bffbc49d2ae5b619559
Albie Baker-Smith <bakersmitha@gmail.com>
2018-02-07 10:08:57 +0000

    S2T Server

--- speech/speech_to_text.py
+++ speech/speech_to_text.py
@@ -6,37 +6,60 @@ from encoders.encode_action import ActionEncoder
 
 import requests
 
+server = "http://192.168.0.101:8080/action"
+
 
 # if __name__ == '__main__':
-#     s = 'go to the first door'.split()
+#     instructions = [
+#         'stop',
+#         'go crouched to lab 300',
+#         'stand up',
+#         'pick up the rock on your right',
+#         'run to the gun range',
+#         'throw the rock',
+#         'go backwards',
+#         'go to the first door on your left',
+#         'run to server room 301',
+#         'go to the door on your right',
+#     ]
 #
-#     if s:
-#         parsed, response, remaining = action().parse(s)
+#     while (True):
+#         for j, instruction in enumerate(instructions):
+#             print('[',j,']',instruction)
 #
-#         action_json = json.loads(json.dumps(parsed, cls=ActionEncoder))
+#         i = int(input("\nindex:"))
 #
-#         print('Sending :', action_json)
-#         server = 'http://192.168.0.16:8080/action'
 #
-#         server_response = requests.post(server, json=action_json)
-#         print(server_response)
+#         print('Parsing')
+#         result = action().parse(instructions[i].split())
 #
-#     else:
-#         print('No parse')
+#         if result:
+#             print('Parsed')
+#
+#             action_json = json.loads(json.dumps(result.parsed, cls=ActionEncoder))
+#
+#             print('Sending :', action_json)
+#             server_response = requests.post(server, json=action_json)
+#             print(server_response)
+#
+#         else:
+#             print('No parse')
+#
+#         print('\n')
 
 
-# # this is called from the background thread
-# def callback(recognizer, audio):
-#     # received audio data, now we'll recognize it using Google Speech Recognition
-#     print('Parsing')
-#     try:
-#         print("Google Speech Recognition thinks you said " + recognizer.recognize_google(audio))
-#     except sr.UnknownValueError:
-#         print("Google Speech Recognition could not understand audio")
-#     except sr.RequestError as e:
-#         print("Could not request results from Google Speech Recognition service; {0}".format(e))
-#
-#
+# this is called from the background thread
+def callback(recognizer, audio):
+    # received audio data, now we'll recognize it using Google Speech Recognition
+    print('Parsing')
+    try:
+        print("Google Speech Recognition thinks you said " + recognizer.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+
 def choose_mic():
     print("Choose a microphone:")
 
@@ -71,7 +94,7 @@ if __name__ == '__main__':
 
     with mic as source:
         print('Adjusting for ambient noise')
-        rec.adjust_for_ambient_noise(mic)
+        rec.adjust_for_ambient_noise(mic, duration = 2)
         print('Done adjusting')
 
     GOOGLE_CLOUD_SPEECH_CREDENTIALS = json.dumps(json.load(open('../google_cloud_speech_credentials.json')))
@@ -95,18 +118,18 @@ if __name__ == '__main__':
 
             if result:
                 print("Sending :", json.dumps(result.parsed, cls=ActionEncoder))
-                server = "http://192.168.0.16:8080/action"
+                server = "http://192.168.0.101:8080/action"
                 requests.post(server, json=json.loads(json.dumps(result.parsed, cls=ActionEncoder)))
 
         except sr.UnknownValueError:
             print("Oops! Didn't catch that")
         except sr.RequestError as e:
             print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
-#
-#         # while True:
-#         #     input()
-#         #     print('Listening')
-#         #     stop_listening = rec.listen_in_background(source, callback)
-#         #     input()
-#         #     print('Stopped')
-#         #     stop_listening()
+
+        # while True:
+        #     input()
+        #     print('Listening')
+        #     stop_listening = rec.listen_in_background(source, callback)
+        #     input()
+        #     print('Stopped')
+        #     stop_listening()
