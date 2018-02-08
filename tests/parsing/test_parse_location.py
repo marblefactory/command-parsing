c62d1a981f3faf75c367dafba9f1da652f92d7e5
Albie Baker-Smith <bakersmitha@gmail.com>
2018-02-08 22:25:24 +0000

    Tested parsing 'live' as 'lab'

--- tests/parsing/test_parse_location.py
+++ tests/parsing/test_parse_location.py
@@ -81,6 +81,10 @@ class AbsoluteTestCase(unittest.TestCase):
         s = 'go to lab 3'.split()
         assert location().parse(s).parsed == Absolute('lab 3')
 
+    def test_parses_live_as_lab(self):
+        s = 'go to live 3'.split()
+        assert location().parse(s).parsed == Absolute('lab 3')
+
     def test_parses_meeting_room(self):
         s = 'go to meeting room 89'.split()
         assert location().parse(s).parsed == Absolute('meeting room 89')
