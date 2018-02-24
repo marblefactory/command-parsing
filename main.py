e3f6f68b9c665ea9daa236c442e34b95b617f896
Albie Baker-Smith <bakersmitha@gmail.com>
2018-02-24 19:40:58 +0000

    Added actual post to server

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from client.client import run_client, mock_post_action_to_server, post_action_to
 from nltk.corpus import wordnet as wn
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from functools import partial
 import os
 
 
@@ -12,7 +13,7 @@ if __name__ == '__main__':
     wn.ensure_loaded()
 
     # Train the ChatBot in case the transcript was not parsed as an action.
-    print('Training chat bot...')
+    print('Training Chat Bot...')
 
     chat_bot = ChatBot('James', read_only=True)
     chat_bot.set_trainer(ChatterBotCorpusTrainer)
@@ -23,4 +24,5 @@ if __name__ == '__main__':
     cwd = os.getcwd()
     failure_responses_dir = os.path.join(cwd, 'client', 'failure_responses')
 
+    #post = partial(post_action_to_server, 'http://192.168.0.30:8080/action')
     run_client(failure_responses_dir, chat_bot, mock_post_action_to_server)
