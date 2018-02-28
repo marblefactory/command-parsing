dbc29a1abb8e03ef28a466c800b80dc5de68ea34
Albie Baker-Smith <bakersmitha@gmail.com>
2018-02-28 16:20:04 +0000

    Added connecting state

--- interface/static/js/state.js
+++ interface/static/js/state.js
@@ -1,11 +1,24 @@
 "use strict";
 
 /**
+ * Plays the sound in the audio element with the given id.
+ */
+function play(id) {
+    var audioElement = document.querySelector(id);
+    audioElement.play();
+}
+
+/**
  * Represents a state that the website can be in.
  */
 class State {
-    constructor(stateDiv) {
+    /**
+     * @param {Element} stateDiv - the div in which text will be displayed.
+     * @param {boolean} shouldClearStateDiv - whether to remove any html inside the div on exiting the state.
+     */
+    constructor(stateDiv, shouldClearStateDiv) {
         this.stateDiv = stateDiv;
+        this.shouldClearStateDiv = shouldClearStateDiv;
         this.eventHandlers = [];
     }
 
@@ -37,7 +50,13 @@ class State {
      * Can be used to perform cleanup such as removing event handlers.
      */
     exitState() {
-        console.log(`NUM = ${this.eventHandlers}`);
+        console.log("HERE");
+        console.log(this.shouldClearStateDiv);
+        if (this.shouldClearStateDiv) {
+            console.log("CLEARED");
+            this.stateDiv.innerHTML = '';
+        }
+
         for (var i=0; i<this.eventHandlers.length; i++) {
             var handlerRef = this.eventHandlers[i];
             document.removeEventListener(handlerRef.event, handlerRef.callback);
@@ -60,11 +79,11 @@ class State {
  */
 class ConnectWaitingState extends State {
     constructor(stateDiv) {
-        super(stateDiv);
+        super(stateDiv, true);
     }
 
     enterState() {
-        this.stateDiv.innerHTML = 'Press any key to connect';
+        this.stateDiv.innerHTML = 'Press any key to start connecting';
         super.addListener('keyup', () => super.segue(ConnectingState));
     }
 }
@@ -74,11 +93,45 @@ class ConnectWaitingState extends State {
  */
 class ConnectingState extends State {
     constructor(stateDiv) {
-        super(stateDiv);
+        super(stateDiv, true);
     }
 
     enterState() {
-        this.stateDiv.innerHTML = "HERE";
+        // The text to display and the time (ms) to wait before showing the next text.
+        var texts = [
+            ['SpySpeakBIOS 4.0 Release 6.0', 450],
+            ['Copyright 2005-2018 SpySpeak Technologies Ltd.', 100],
+            ['', 450],
+            ['BIOS version 29.02', 100],
+            ['Gateway Solo 9550', 120],
+            ['System Id = 513', 110],
+            ['Build Time; 09/10/2017', 200],
+            ['', 550],
+            ['639 KB System RAM Passed', 70],
+            ['254 KB Extended RAM Passed', 100],
+            ['512 K Cache Passed', 90],
+            ['System BIOS Shadowed', 0],
+            ['', 50],
+            ['Connecting to host...', 850],
+            ['Done connecting', 500]
+        ];
+
+        function addTexts(stateDiv, index, callback) {
+            if (index === texts.length) {
+                callback();
+                return;
+            }
+
+            var text = `${texts[index][0]}<br/>`;
+            var waitTime = texts[index][1];
+            stateDiv.innerHTML += text;
+
+            setTimeout(() => addTexts(stateDiv, index + 1, callback), waitTime);
+        }
+
+        // Start the animation.
+        play('#fax_machine');
+        addTexts(this.stateDiv, 0, () => console.log("DONE"));
     }
 }
 
