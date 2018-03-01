7ad30e6a0bb64acf3319bf11ed8e8849f7c1152d
Albie Baker-Smith <bakersmitha@gmail.com>
2018-03-01 23:19:02 +0000

    Added SpeechResponder

---
+++ interface/speech_responder.py
@@ -0,0 +1,57 @@
+from parsing.parser import Parser, strongest
+from parsing.pre_processing import pre_process
+from parsing.parse_result import SuccessParse, PartialParse, FailureParse
+from actions.action import Action
+from typing import Optional, Callable
+
+
+class SpeechResponder:
+    """
+    Parses the transcript and creates a speech response to send to the user.
+    Depending on the **state** the response will be different, for example, if the last transcript gave a partial
+    response, we will try and parse with the failed parser.
+    """
+
+    # Is a parser if the last transcript gave a partial parse. The parser will be tried on the next parse.
+    _partial: Optional[Parser]
+
+    def __init__(self, parser: Parser,
+                 parsed_response: Callable[[Action], str],
+                 no_parsed_response: Callable[[str], str]):
+        """
+        :param parser: the parser to be used when parsing the transcript.
+        :param parsed_response: function used to create a response when an action was parsed from the transcript.
+        :param no_parsed_response: function used to create a response when nothing could be parsed from the transcript.
+        """
+        self.parser = parser
+        self.parsed_response = parsed_response
+        self.no_parsed_response = no_parsed_response
+        self._partial = None
+
+    def parse(self, transcript: str) -> (str, Optional[Action]):
+        """
+        :param transcript: the transcript of the user's speech.
+        :return: a speech response to be sent to the client to speak. An action for the spy to perform may optionally
+                 be returned if one was parsed from the transcript.
+        """
+
+        # Create a parser containing the partial parser if there was a partial result last time.
+        parser = strongest([self._partial, self.parser]) if self._partial else self.parser
+
+        words = pre_process(transcript)
+        result = parser.parse(words)
+
+        if isinstance(result, SuccessParse):
+            self._partial = None
+            action = result.parsed
+            return (self.parsed_response(action), action)
+
+        elif isinstance(result, PartialParse):
+            self._partial = result.failed_parser
+            return ('where?', None)
+
+        elif isinstance(result, FailureParse):
+            self._partial = None
+            return (self.no_parsed_response(transcript), None)
+
+        raise RuntimeError('unexpected ParseResult type')
---
+++ tests/interface/test_speech_responder.py
@@ -0,0 +1,71 @@
+import unittest
+from interface.speech_responder import SpeechResponder
+from parsing.parser import *
+
+
+class SpeechResponderTestCase(unittest.TestCase):
+    def responder(self) -> SpeechResponder:
+        """
+        :return: a speech responder which parses the transcript 'hello world' partially.
+                 'success' is the speech response returned for successful parsing, and 'failure' is the speech response
+                 for failed parsing.
+        """
+        def combine(parsed: str, response: Response) -> Parser:
+            return partial_parser(word_match('world').map_parsed(lambda x: parsed + x), 0.5)
+
+        # A parser which parses 'hello world', but where 'world' can be asked for after (i.e. partial parsing).
+        parser = word_match('hello').then(combine)
+
+        return SpeechResponder(parser, lambda _: 'success', lambda _: 'failure')
+
+    def test_generates_success_speech(self):
+        responder = self.responder()
+
+        speech, parsed = responder.parse('hello world')
+        assert speech == 'success'
+
+    def test_generates_parsed_object(self):
+        responder = self.responder()
+
+        speech, parsed = responder.parse('hello world')
+        assert parsed == 'helloworld'
+
+    def test_generates_partial_speech(self):
+        responder = self.responder()
+
+        speech, parsed = responder.parse('hello')
+        assert speech == 'where?'
+
+    def test_generates_no_partial_parsed_object(self):
+        responder = self.responder()
+
+        speech, parsed = responder.parse('hello')
+        assert parsed is None
+
+    def test_parses_partial_success_speech(self):
+        # Tests that after a partial parse, the full object can be parsed after.
+        responder = self.responder()
+        _ = responder.parse('hello')
+        speech, parsed = responder.parse('world')
+
+        assert speech == 'success'
+
+    def test_parses_partial_success_object(self):
+        # Tests that after a partial parse, the full object can be parsed after.
+        responder = self.responder()
+        _ = responder.parse('hello')
+        speech, parsed = responder.parse('world')
+
+        assert parsed == 'helloworld'
+
+    def test_generates_failure_speech(self):
+        responder = self.responder()
+
+        speech, parsed = responder.parse('nothing here')
+        assert speech == 'failure'
+
+    def test_generates_no_failure_parsed_object(self):
+        responder = self.responder()
+
+        speech, parsed = responder.parse('nothing here')
+        assert parsed is None
