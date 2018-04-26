e86b15d6943b56a2a85a0ebd65ee715e7038ed49
Albie Baker-Smith <bakersmitha@gmail.com>
2018-04-26 15:09:30 +0100

    main function

--- speech_server.py
+++ speech_server.py
@@ -1,10 +1,12 @@
 from app import app, socketio, GAME_MODE, FILL_CACHE, preload
 
-if not GAME_MODE:
-    print("WARNING: Not in Game Mode")
 
-# Filling the cache takes a long time as all the tests have to run.
-preload(fill_cache=FILL_CACHE)
+if __name__ == '__main__':
+    if not GAME_MODE:
+        print("WARNING: Not in Game Mode")
 
-print('Running Server')
-socketio.run(app, host='0.0.0.0', port=5000)
+    # Filling the cache takes a long time as all the tests have to run.
+    preload(fill_cache=FILL_CACHE)
+
+    print('Running Server')
+    socketio.run(app, host='0.0.0.0', port=5000)
